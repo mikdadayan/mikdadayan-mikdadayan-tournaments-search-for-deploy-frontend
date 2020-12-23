2fc7835fea7f96af062e8cc38d644ab2f454f5b2
Mik Dadayan <dadayan.mikayel@gmail.com>
2020-12-23 14:51:58 +0400

    Fix Service Worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -20,6 +20,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+registerServiceWorker();
 reportWebVitals();
-serviceWorker.unregister();
+// serviceWorker.unregister();
